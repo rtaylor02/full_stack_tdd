e790a66128b58c90b70e1dda279727fd3ba9afc0
rtaylor02 <taylor.rod.j@gmail.com>
2023-01-10 14:29:38 +0000

    Added spring initializr project into backend folder

---
+++ backend/full_stack_tdd/src/main/java/com/rodtaylor02/full_stack_tdd/FullStackTddApplication.java
@@ -0,0 +1,13 @@
+package com.rodtaylor02.full_stack_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackTddApplication.class, args);
+	}
+
+}
---
+++ backend/full_stack_tdd/src/test/java/com/rodtaylor02/full_stack_tdd/FullStackTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodtaylor02.full_stack_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
