5cf5409df2e74b7d15c8069e9a782f5d8234d500
rtaylor02 <taylor.rod.j@gmail.com>
2023-01-11 09:25:00 +0000

    Added skeleton on ItemController.java and Item.java

---
+++ backend/full_stack_tdd/src/main/java/com/rodtaylor02/full_stack_tdd/controllers/ItemController.java
@@ -0,0 +1,9 @@
+package com.rodtaylor02.full_stack_tdd.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+
+}
---
+++ backend/full_stack_tdd/src/main/java/com/rodtaylor02/full_stack_tdd/model/Item.java
@@ -0,0 +1,9 @@
+package com.rodtaylor02.full_stack_tdd.model;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Item {
+
+
+}
